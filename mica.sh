c2f599de6789ba81cf2f5ac2740642e5a8e49643
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2025-12-31 16:25:15 +0530

    Update mica.sh

--- mica.sh
+++ mica.sh
@@ -15,7 +15,7 @@ rm -rf hardware/oplus
 rm -rf .repo/local_manifests
 
 #start rom build
-repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs && \
+repo init -u https://github.com/Project-Mica/manifest -b 16-qpr2 && \
 
 # Resync sources
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
@@ -24,7 +24,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh 
 
 #clone device tree
-git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b lineage-23.1 device/oneplus/larry && \
+git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b mica_os device/oneplus/larry && \
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_sm6375-common.git -b sixteen-qpr1 device/oneplus/sm6375-common && \
 
 #clone vendor tree
@@ -33,18 +33,23 @@ git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_sm6375-comm
 
 #clone kernel and hardware tree
 git clone https://github.com/DEMONTHUNDER/android_kernel_oneplus_sm6375.git -b sixteen-qpr1 kernel/oneplus/sm6375 && \
-git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b sixteen-qpr1 hardware/oplus && \
+git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b lineage23.2 hardware/oplus && \
 
 # Setup the build environment
 . build/envsetup.sh
 echo "Environment setup success."
 
+
+#gms 
+cd vendor/gms
+bash generate-gms.sh
+
 # Lunch before building
-lunch lineage_larry-bp3a-userdebug
+lunch mica_larry-bp4a-userdebug
 echo "Lunch command executed."
 
 # Build ROM
 echo "========================="
 echo "Starting ROM Compilation..."
 echo "========================="
-m evolution -j$(nproc --all)
+m mica-release -j$(nproc --all)
