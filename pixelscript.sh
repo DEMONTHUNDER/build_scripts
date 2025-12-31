da00de479eb844e6c83da1f1cd3fc30fe12c5492
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2025-12-31 11:57:22 +0530

    Update pixelscript.sh

--- pixelscript.sh
+++ pixelscript.sh
@@ -1,13 +1,30 @@
-
+#!/bin/bash
 
 repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs && \
-/opt/crave/resync.sh && \
+
+# Resync sources
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh 
+
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b pixelos16 device/oneplus/larry && \
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_sm6375-common_austen.git -b sixteen-qpr1 device/oneplus/sm6375-common && \
 git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_larry.git -b sixteen-qpr1 vendor/oneplus/larry && \
 git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_sm6375-common.git -b sixteen-qpr1 vendor/oneplus/sm6375-common && \
 git clone https://github.com/DEMONTHUNDER/android_kernel_oneplus_sm6375.git -b sixteen-qpr1 kernel/oneplus/sm6375 && \
 git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b sixteen-qpr1 hardware/oplus && \
-. build/envsetup.sh && \
-lunch custom_larry-bp3a-user && \
-m pixelos -j$(nproc --all)"
+
+ # Setup the build environment
+ . build/envsetup.sh
+ echo "Environment setup success."
+
+ # Lunch before building
+ lunch custom_larry-bp3a-user
+ echo "Lunch command executed."
+
+ # Build ROM
+ echo "========================="
+ echo "Starting ROM Compilation..."
+ echo "========================="
+ m pixelos -j$(nproc --all)
