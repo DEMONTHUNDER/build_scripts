2718e464c4cc2f19107c76bd6f10167b17b2745f
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2025-12-31 12:27:20 +0530

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,6 +1,5 @@
-crave run --projectID 35 --no-patch -- "
-
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs && \
+#!/bin/bash
+repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs && \
 
 # Resync sources
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
@@ -8,7 +7,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh 
 
-git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b pixel-os device/oneplus/larry && \
+git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b lineage-23.1 device/oneplus/larry && \
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_sm6375-common.git -b sixteen-qpr1 device/oneplus/sm6375-common && \
 git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_larry.git -b sixteen-qpr1 vendor/oneplus/larry && \
 git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_sm6375-common.git -b sixteen-qpr1 vendor/oneplus/sm6375-common && \
@@ -20,8 +19,8 @@ git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b sixteen-
 echo "Environment setup success."
 
 # Lunch before building
-lunch custom_larry-bp3a-user
+lunch lineage_larry-bp3a-userdebug
 echo "Lunch command executed."
 
 # Build ROM
-m pixelos -j$(nproc --all)"
+m evolution -j$(nproc --all)"
