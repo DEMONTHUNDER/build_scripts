97cab0a5aa25ae1c76787a4b8e54684baef14542
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2026-09-15 22:59:18 +0530

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -25,6 +25,8 @@ rm -rf vendor/gms .repo/projects/vendor/gms.git
 echo -e "\n${BLUE}➜ [PHASE 3/5] Syncing Evolution X Repositories...${NC}"
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 /opt/crave/resync.sh
+repo sync -c -j$(nproc --all) --force-sync --force-remove-dirty --no-clone-bundle --no-tags
+/opt/crave/resync.sh
 echo -e "${GREEN}✔ Sync Complete.${NC}"
 
 # ========================================================
