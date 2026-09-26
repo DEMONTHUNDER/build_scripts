1fc9a3fc99c30a42f9f203c99867bf13343a9db7
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2026-09-26 16:20:40 +0530

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -42,10 +42,10 @@ echo -e "${GREEN}✔ Sync Complete.${NC}"
 echo -e "\n${BLUE}➜ [PHASE 4/5] Downloading Device Trees...${NC}"
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_larry.git -b a17evox device/oneplus/larry --depth=1
 git clone https://github.com/DEMONTHUNDER/android_device_oneplus_sm6375-common.git -b a17 device/oneplus/sm6375-common --depth=1
-git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_larry.git -b a17 vendor/oneplus/larry --depth=1
-git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_sm6375-common.git -b a17 vendor/oneplus/sm6375-common --depth=1
-git clone https://github.com/DEMONTHUNDER/android_kernel_oneplus_sm6375.git -b a17 kernel/oneplus/sm6375 --depth=1
-git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b a17 hardware/oplus --depth=1
+git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_larry.git -b lineage-23.2 vendor/oneplus/larry --depth=1
+git clone https://github.com/DEMONTHUNDER/proprietary_vendor_oneplus_sm6375-common.git -b lineage-23.2 vendor/oneplus/sm6375-common --depth=1
+git clone https://github.com/DEMONTHUNDER/android_kernel_oneplus_sm6375.git -b lineage-23.2 kernel/oneplus/sm6375 --depth=1
+git clone https://github.com/DEMONTHUNDER/android_hardware_oplus.git -b lineage-23.2 hardware/oplus --depth=1
 echo -e "${GREEN}✔ All repositories cloned successfully.${NC}"
 
 # ============================================================
